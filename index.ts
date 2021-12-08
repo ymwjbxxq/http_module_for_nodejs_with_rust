72e93c2a192a9244ae32f2e7bdf9cfc3ac97cf07
Daniele <>
2021-12-08 16:24:43 +0100

    add typescript

---
+++ index.ts
@@ -0,0 +1,46 @@
+const rust = require("./native/index.node");
+import fetch from 'node-fetch';
+
+const LOOP_COUNT = 10;
+
+export const startExecution: any = async (): Promise<void> => {
+  let now = new Date();
+  console.log("START native", now);
+  const native_promise = [];
+  for (let index = 0; index < LOOP_COUNT; index++) {
+    native_promise.push(native_fetch());
+  }
+  await Promise.all(native_promise);
+  let diffInMs = new Date().getTime() - now.getTime();
+  console.log(`END*********${diffInMs}ms`);
+
+  now = new Date();
+  console.log("START node-fetch", now);
+  const node_promise = [];
+  for (let index = 0; index < LOOP_COUNT; index++) {
+    node_promise.push(node_fetch());
+  }
+  await Promise.all(node_promise);
+  diffInMs = new Date().getTime() - now.getTime();
+  console.log(`END*********${diffInMs}ms`);
+};
+
+async function native_fetch() {
+  const request = {
+    url: "https://hnpqzz25rh.execute-api.eu-central-1.amazonaws.com/test",
+    method: "GET",
+  }
+  const response = rust.get(request);
+  console.log(JSON.parse(response.data));
+}
+
+async function node_fetch() {
+  const response = await fetch('https://hnpqzz25rh.execute-api.eu-central-1.amazonaws.com/test');
+  const body = await response.json();
+
+  console.log(body);
+}
+
+(async () => {
+  await startExecution();
+})();
